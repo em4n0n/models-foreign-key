3f927db24302926c4840286c264592fba58ec8ac
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-25 12:18:41 -0700

    Create a Drinks class

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class DrinksCategory(models.Model):
     category_name = models.CharField(max_length=200)
+    
+class Drinks(models.Model):
+    drink = models.CharField(max_length=200)
+    prince = models.IntegerField()
+    
